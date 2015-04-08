b36357360754bc3c1c6bd970d0bd767208e07ea4
CKAN User <ckan@localhost.localdomain>
2015-04-08 08:17:59 -0700

    first commit

---
+++ ckanext/semre/plugin.py
@@ -0,0 +1,38 @@
+import ckan.plugins as plugins
+import ckan.plugins.toolkit as toolkit
+import ckan.model.package_relationship as PackageRelationship
+import requests
+import urllib, urllib2, httplib
+import re
+import json
+
+# Use this function in custom template to get dataset relationships
+# It will enable us to represent them semantically
+def semre_create(datasetName):
+ 
+    # To get dataset relationships we call CKAN core function 'package_relationship_list'
+    # to whom we forward the name of the dataset
+    datasetRelationships = toolkit.get_action('package_relationships_list')(
+        data_dict={'id': datasetName})
+    
+    # return results as dictionary
+    return datasetRelationships
+
+# Class which implements interface ITemplateHelpers to make our extension function available
+# in CKAN template and interface IConfigurer to tell CKAN that our extension provides
+# new custom template for RDF description of dataset
+class SemRePlugin(plugins.SingletonPlugin):
+    plugins.implements(plugins.ITemplateHelpers)
+
+    plugins.implements(plugins.IConfigurer)
+
+
+    def update_config(self, config):
+
+        # Add this plugin's templates dir to CKAN's extra_template_paths, so
+        # that CKAN will use this plugin's custom templates.
+        toolkit.add_template_directory(config, 'templates_legacy')
+
+    def get_helpers(self):
+        return {'semre_create': semre_create}
+
---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.1'
+
+setup(
+	name='ckanext-semre',
+	version=version,
+	description="Semantic representation of CKAN dataset relationships",
+	long_description="""\
+	""",
+	classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+	keywords='',
+	author='Petar Milic',
+	author_email='milicpetar86@gmail.com',
+	url='',
+	license='open',
+	packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+	namespace_packages=['ckanext', 'ckanext.semre'],
+	include_package_data=True,
+	zip_safe=False,
+	install_requires=[
+		# -*- Extra requirements: -*-
+	],
+    entry_points='''
+        [ckan.plugins]
+        semre=ckanext.semre.plugin:SemRePlugin
+    ''',
+)
